3e63ecf3f4d291ed398fc5b6fe9134730cab4a42
ART89-prog <dykpgilkom@mail.ru>
2023-01-05 21:16:24 +0300

    666

--- js/scripts_new.js
+++ js/scripts_new.js
@@ -15,6 +15,37 @@ $(() => {
 
 
 
+
+    // Фото с маркерами
+	$('.image .btn').click(function(e){
+		e.preventDefault()
+
+		let parent = $(this).closest('.image')
+		let info = $(this).data('info')
+
+		if( $(this).parents('.item').hasClass('active') ) {
+			parent.find('.item').removeClass('active')
+			parent.find('.info').hide()
+		} else {
+			parent.find('.item').removeClass('active')
+			parent.find('.info').hide()
+
+			$(this).parents('.item').addClass('active')
+			parent.find(info).fadeIn(300)
+		}
+	})
+
+	$('.image .close').click(function(e){
+		e.preventDefault()
+
+		let parent = $(this).closest('.image')
+
+		parent.find('.item').removeClass('active')
+		parent.find('.info').hide()
+	})
+
+
+
 	if ('function' === typeof MediaPlayer) {
 		[].forEach.call(document.querySelectorAll('audio[controls]'), function (media) {
 			player = media.player = new MediaPlayer(media, {
