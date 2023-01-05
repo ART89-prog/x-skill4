365e370f7c6b23fbcf60698b6ade0fb70559fb0b
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2023-01-05 16:54:56 +0300

    444

--- js/scripts_new.js
+++ js/scripts_new.js
@@ -20,6 +20,19 @@ $(() => {
       });
 
 
+
+	if ('function' === typeof MediaPlayer) {
+		[].forEach.call(document.querySelectorAll('audio[controls]'), function (media) {
+			player = media.player = new MediaPlayer(media, {
+				svgs: {
+					play: 'images/sprite.svg#ic_play',
+					pause: 'images/sprite.svg#ic_pause',
+				},
+			})
+		})		
+	}
+
+
     $(document).on('change', '.error', function() {
         $(this).removeClass('error');
     })
