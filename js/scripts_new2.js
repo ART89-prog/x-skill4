c5854fe50d7477986a7cf79298f34c22cf1d6285
ART89-prog <dykpgilkom@mail.ru>
2023-01-05 22:52:27 +0300

    777

---
+++ js/scripts_new2.js
@@ -0,0 +1,36 @@
+$(() => {
+
+    // Фото с маркерами
+	$('.image .btn').click(function(e){
+		e.preventDefault()
+
+		let parent = $(this).closest('.image')
+		let info = $(this).data('info')
+
+		if( $(this).parents('.item').hasClass('active') ) {
+			parent.find('.item').removeClass('active')
+			parent.find('.info').hide()
+		} else {
+			parent.find('.item').removeClass('active')
+			parent.find('.info').hide()
+
+			$(this).parents('.item').addClass('active')
+			parent.find(info).fadeIn(300)
+		}
+	})
+
+	$('.image .close').click(function(e){
+		e.preventDefault()
+
+		let parent = $(this).closest('.image')
+
+		parent.find('.item').removeClass('active')
+		parent.find('.info').hide()
+	})
+
+
+
+    
+
+
+})
